6453475d49f582fe7bc70c859eb976c920745c41
Wanderson Brito Santos <wbsmsn@hotmail.com>
2025-05-18 12:51:42 -0300

    projeto finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,182 +1,120 @@
+const f_number=document.getElementById("f_number");
+const val_1=document.getElementById("val_in");
+const val_2=document.getElementById("val_fin");
+const res=document.getElementById("res");
+const reset=document.getElementById("btn_reset");
+//let arraycriado=false;
 
-const f_number = document.getElementById("f_number");
-//const clicado = 
-
-
-/*
-const function_cliacado = document.addEventListener('keyup',(event)=>{
-    event.preventDefault();
-
-  if (event.key === 13) { 
-    event.preventDefault();
-   
-    console.log('Tecla Enter pressionada');
-    /*console.log(valor_aposta)
-    */
- /* }
-});
-
-
-*/
-
-/**
-
-const analizar_number=()=>{
-  const valor_aposta = f_number.value;
-  if(valor_aposta == ""){
-      p.innerHTML =" oi"
-  }
+const criar_array=(val_1,val_2)=>{
+ 
+    const val1=parseInt(val_1.value);
+    const val2=parseInt(val_2.value);
+    let listNumber=[];
+    for(let i=val1;i<=val2;i++){//cria array dos intervalos dos numeros 
+      listNumber.push(i);
+    }
+    const iAleatorio=Math.floor(Math.random()*listNumber.length)//um indice aleatorio dos numeros gerados 
+    const valor_posInd=listNumber[iAleatorio]// captura o valor no indice aleatorio 
+  // consult_array(posInd);//envia o valor que esta na posicao do indice para comparação 
+    comparaResult(valor_posInd)
 
 }
 
+const comparaResult=(vpi)=>{
+ const numero_sorte=parseInt(f_number.value)
+ const posval=vpi;
+ console.log(posval +' '+ numero_sorte)
+ if(numero_sorte!=vpi){
+  res.style.display ="flex";
+  res.innerHTML = "Não foi dessa vez! Seu número não foi sorteado no intervalo informado."
+  setTimeout(()=>{
+    res.style.display="none";
+    },5000);
+ }else{
+  res.style.display ="flex";
+  res.innerHTML = "Parabéns! Seu número foi sorteado!"
+  setTimeout(()=>{
+    res.style.display="none";
+    },5000);
+ }
+}
 
+const imprime_erros=(n)=>{
+  res.style.display ="flex";
 
-*/
-
-
-
-
-
-const cliacado = document.addEventListener('keyup', function(event) {
-  event.preventDefault();
+  switch(n){
+    case 0:
+       res.innerHTML = "Por favor, preencha o número da sorte."
+    break;  
+    case 1:
+       res.innerHTML = "Informe o valor inicial do intervalo."
+   break;  
+   case 2:
+       res.innerHTML = "Informe o valor final do intervalo."
+   break;
+   case 3:
+    res.innerHTML = "O valor inicial deve ser menor que o valor final."
+  break; 
+   case 4:
+       res.innerHTML = "O número da sorte está abaixo do intervalo escolhido."
+   break;
+   case 5:
  
-  if (event.keyCode === 13) { 
-    
-  //  let valor_aposta = f_number.value;
-    console.log('Tecla Enter pressionada');
-    //console.log(valor_aposta);
+     default:
+    break
   }
 
-  event.preventDefault();
+  setTimeout(()=>{
+    res.style.display="none";
+    },5000);
+}
+
+const Erros=(f_number,val_1,val_2)=>{
+
+  if(f_number.value==""){
+     imprime_erros(0)
+     return
+  }else if(val_1.value==""){
+    imprime_erros(1)
+    return
+  }else if(val_2.value==""){
+    imprime_erros(2)
+    return
+  }
+  
+  const n_aposta=parseInt(f_number.value);
+  const f_val1=parseInt(val_1.value);
+  const f_val2=parseInt(val_2.value);
+  if(f_val1>f_val2){
+    imprime_erros(3)
+    return
+  }else if(n_aposta<f_val1){
+    imprime_erros(4)
+    return
+  } else if(n_aposta >f_val2){
+    imprime_erros(5)
+    return
+  }
+  else{
+    criar_array(val_1,val_2)
+    return;
+  }
+}
+
+ reset.addEventListener("click",()=>{
+    location.reload()
+})
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault(); //  Impede o submit automático
+  }
 });
 
-/***
-const f_number=document.getElementById("f_number");// pega o campo de numero enviado pelo usuario
-const p=document.getElementById("res");
+document.addEventListener('keyup',(event)=>{
 
-/***
- *    <p id="res"> Os campos de faixa não foram prenchidos, favor preencher.</p>
-                    <!-- <1<p id="res"> O número escolhido e superior a faixa de possivel número sorteado.</p>
-                    <p id="res"> O número escolhido e inferior a faixa de possiveis números sorteado.<p>
-
-                    <p id="res"> Lamento! O números  "var" escolhido não condiz com o número secreto</p> -->
-                    <!--
- */
-                 //   res.innerHTML += '<p>Outro parágrafo aqui</p>';
-
-
-
-
-/*
-btn_C.addEventListener("click", () => {
-    if (C_nome.value == "" || C_tel.value == "") {
-      alert(" Os campos de nome ou telefone não pode esta vazios!");
-      C_nome.value = "";
-      C_tel.value = " ";
-    } else {
-      const nome = C_nome.value;
-      const tel = C_tel.value;
-      C_nome.value = "";
-      C_tel.value = " ";
-      registro_nomo_tel(nome, tel);
-    }
-  });
-  
-  const registro_nomo_tel = (n, t) => {
-    const nome = n;
-    const tel = t;
-    p_back.innerHTML = `Nome: ${nome} e Telefone: ${tel} adicionados com sucesso!`;
-  
-    if (i % 2 === 0) {
-      let trow = document.createElement("tr");
-      trow.className = "section table tr";
-  
-      let tdado_nome = document.createElement("td");
-      let tdado_tel = document.createElement("td");
-  
-      tdado_nome.textContent = nome;
-      tdado_tel.textContent = tel;
-  
-      trow.appendChild(tdado_nome);
-  
-      trow.appendChild(tdado_tel);
-  
-      t_boby.appendChild(trow);
-      console.log(i);
-    } else {
-      let trow = document.createElement("tr");
-      trow.className = "tr2";
-  
-      let tdado_nome = document.createElement("td");
-      let tdado_tel = document.createElement("td");
-  
-      tdado_nome.textContent = nome;
-      tdado_tel.textContent = tel;
-  
-      trow.appendChild(tdado_nome);
-  
-      trow.appendChild(tdado_tel);
-  
-      t_boby.appendChild(trow);
-    }
-    i++;
-    setTimeout(() => (p_back.innerHTML = ""), 3000);
-  };**/ 
-
-
-
-/*
-btn_C.addEventListener("click", () => {
-    if (C_nome.value == "" || C_tel.value == "") {
-      alert(" Os campos de nome ou telefone não pode esta vazios!");
-      C_nome.value = "";
-      C_tel.value = " ";
-    } else {
-      const nome = C_nome.value;
-      const tel = C_tel.value;
-      C_nome.value = "";
-      C_tel.value = " ";
-      registro_nomo_tel(nome, tel);
-    }
-  });
-  
-  const registro_nomo_tel = (n, t) => {
-    const nome = n;
-    const tel = t;
-    p_back.innerHTML = `Nome: ${nome} e Telefone: ${tel} adicionados com sucesso!`;
-  
-    if (i % 2 === 0) {
-      let trow = document.createElement("tr");
-      trow.className = "section table tr";
-  
-      let tdado_nome = document.createElement("td");
-      let tdado_tel = document.createElement("td");
-  
-      tdado_nome.textContent = nome;
-      tdado_tel.textContent = tel;
-  
-      trow.appendChild(tdado_nome);
-  
-      trow.appendChild(tdado_tel);
-  
-      t_boby.appendChild(trow);
-      console.log(i);
-    } else {
-      let trow = document.createElement("tr");
-      trow.className = "tr2";
-  
-      let tdado_nome = document.createElement("td");
-      let tdado_tel = document.createElement("td");
-  
-      tdado_nome.textContent = nome;
-      tdado_tel.textContent = tel;
-  
-      trow.appendChild(tdado_nome);
-  
-      trow.appendChild(tdado_tel);
-  
-      t_boby.appendChild(trow);
-    }
-    i++;
-    setTimeout(() => (p_back.innerHTML = ""), 3000);
-  };**/
+  if(event.key === "Enter"){
+     // event.preventDefault();
+     
+       Erros(f_number,val_1,val_2)
+  };
+});
